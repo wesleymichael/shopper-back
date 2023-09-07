a19b5300e2250f9caff08277a639a28eb4ca73df
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-09-07 18:42:48 -0300

    refactor: change default port from 3000 to 4000

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import app from './app';
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is running and up on port: ${port}`);
